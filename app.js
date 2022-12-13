e60cae9e72367acaee8ab960aa2f1c8d64875776
imtuda.dev <120447477+imtuda4@users.noreply.github.com>
2022-12-13 14:39:03 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const keys = document.querySelectorAll('.key')
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return
+    audio.currentTime = 0
+    audio.play()
+    key.classList.add('playing')
+}
+
+function removePlaying(e) {
+    if(e.propertyName !== 'transform') return
+    this.classList.remove('playing')
+}
+
+keys.forEach(key => {
+    key.addEventListener('transitionend', removePlaying)
+})
+
+window.addEventListener('keydown', playSound)
+
